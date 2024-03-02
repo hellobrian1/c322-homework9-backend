ce2de3e74e35fa2c1185f6bdb3662c39441ff8f3
brilord <143294125+hellobrian1@users.noreply.github.com>
2024-03-01 20:42:17 -0500

    Update C322Spring2024Homework2Application.java

--- src/main/java/c322spring2024homework2/c322spring2024homework2/C322Spring2024Homework2Application.java
+++ src/main/java/c322spring2024homework2/c322spring2024homework2/C322Spring2024Homework2Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class C322Spring2024Homework2Application {
-
+//
 	public static void main(String[] args) {
 		SpringApplication.run(C322Spring2024Homework2Application.class, args);
 	}
